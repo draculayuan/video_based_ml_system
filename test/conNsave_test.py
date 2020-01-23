175cd01d5e69ee5b23e514960213d72ab2060c1c
draculayuan <liamliuyuan@yuanblog.asia-southeast1-b.c.clean-evening-198907.internal>
2020-01-23 07:09:55 +0000

    update

--- test/conNsave_test.py
+++
@@ -1,9 +0,0 @@
-from consumeANDsave import consume
-
-db_info = {
-        'host': 'localhost',
-        'user': 'yuan',
-        'passwd': 'Liuyuan980820',
-        'db': 'sentiment'
-        }
-consume('display', 'earliest', db_info)
